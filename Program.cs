ea60da46339f9040483d7b87f62ec4420aee3982
VIVANK215480 <minhq4963@gmail.com>
2024-09-09 15:12:19 +0700

    Add files via upload
    CStessDLL

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LIBDLL;
+
+namespace CStestDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số tiền gốc: ");
+            double principal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhập lãi suất hàng năm (%): ");
+            double interestRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhập số năm: ");
+            int years = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calculator = new Calculator();
+            double totalAmount = calculator.CalculateTotalAmount(principal, interestRate, years);
+            double interest = calculator.CalculateInterest(principal, interestRate, years);
+
+            Console.WriteLine($"Tổng số tiền sau {years} năm: {totalAmount:F2}");
+            Console.WriteLine($"Tổng tiền lãi: {interest:F2}");
+        }
+    }
+}
