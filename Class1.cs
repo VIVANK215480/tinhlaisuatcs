92cb77b78db74d9a1ab7f40b61dbec392ce048ab
VIVANK215480 <minhq4963@gmail.com>
2024-09-09 15:13:01 +0700

    Add files via upload
    LIBDLL

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIBDLL
+{
+    using System;
+        public class Calculator
+        {
+            public double CalculateTotalAmount(double principal, double interestRate, int years)
+            {
+                return principal * Math.Pow((1 + interestRate / 100), years);
+            }
+
+            public double CalculateInterest(double principal, double interestRate, int years)
+            {
+                double totalAmount = CalculateTotalAmount(principal, interestRate, years);
+                return totalAmount - principal;
+            }
+        }
+    }
+
